1e26bc5a6bf7992b0ef29e5f836199b0ac0fd909
Faiz Khan <140372636+calvincandiec137@users.noreply.github.com>
2025-12-26 17:48:48 +0000

    Delete .next/dev directory

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__51d697a8._.css",
-  "static/chunks/_5ba45846._.js"
-],
-    source: "dynamic"
-});
