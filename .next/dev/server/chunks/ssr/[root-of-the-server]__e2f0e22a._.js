60780d312c8997853ba038617dfb416e632ad394
faiz <khanfaizmominshama@gmail.com>
2025-12-26 23:16:26 +0530

    backup

--- .next/dev/server/chunks/ssr/[root-of-the-server]__e2f0e22a._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__e2f0e22a._.js
@@ -37,7 +37,7 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$lucide$2d$re
 ;
 function ResumeButton() {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("a", {
-        href: "https://drive.google.com/file/d/1ptrZPY8opSJo1rgHU4A0jIzCi74aqWzl/view?usp=drive_link",
+        href: "https://drive.google.com/file/d/1kfMYh8I5mL1fe_sbnwl5ObCJykFxXQcw/view?usp=sharing",
         target: "_blank",
         rel: "noopener noreferrer",
         "aria-label": "Download resume",
--- .next/dev/static/chunks/components_resume-button_tsx_b37fc44c._.js
+++ .next/dev/static/chunks/components_resume-button_tsx_b37fc44c._.js
@@ -13,7 +13,7 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$lucide$2d$re
 ;
 function ResumeButton() {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("a", {
-        href: "https://drive.google.com/file/d/1ptrZPY8opSJo1rgHU4A0jIzCi74aqWzl/view?usp=drive_link",
+        href: "https://drive.google.com/file/d/1kfMYh8I5mL1fe_sbnwl5ObCJykFxXQcw/view?usp=sharing",
         target: "_blank",
         rel: "noopener noreferrer",
         "aria-label": "Download resume",
