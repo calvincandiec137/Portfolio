0715adda01b402ecbcabf4a95f1c597df8da1f12
faiz <khanfaizmominshama@gmail.com>
2026-02-16 19:53:04 +0530

    updated resume

--- components/resume-button.tsx
+++ components/resume-button.tsx
@@ -5,7 +5,7 @@ import { Download } from "lucide-react"
 export default function ResumeButton() {
   return (
     <a
-      href="https://drive.google.com/file/d/1s9lqvgADR3gx6GMGItsMTDW5hR1jSUHK/view?usp=sharing"
+      href="https://drive.google.com/file/d/1t1keNQGPA77R0TYzTJX-Xt8bYmQSEuS6/view?usp=sharing"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Download resume"
